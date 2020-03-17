1b3779fb58d0e4449c4d7e51f6a2590a42cfa9f4
Toprak <hello@toprak.dev>
2020-03-17 21:53:51 +0300

    added apollo-boost

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -1,13 +1,34 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
 
 export default function SwipeScreen() {
+    const client = new ApolloClient({
+      uri: 'https://www.graphqlhub.com/graphql',
+    });
+
+    client.query({
+        query: gql `{
+            reddit {
+                subreddit(name: "darkmeme"){
+                    hotListings(limit: 19) {
+                            fullnameId
+                            title
+                            score
+                            url
+                        }
+                    }
+            }
+        }`
+    }).then(result => console.log(result))
+
 
     return (
         <View>
             <Text> Like me! </Text>
         </View>
-);
+    );
 }
 
 SwipeScreen.navigationOptions = {
