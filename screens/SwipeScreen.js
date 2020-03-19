8303158fb9ae47f24a55e0b56ed1de69bb93fd10
Toprak <hello@toprak.dev>
2020-03-19 23:02:41 +0300

    updated screen page to component structure

--- screens/SwipeScreen.js
+++ screens/SwipeScreen.js
@@ -1,36 +1,66 @@
-import * as React from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import ApolloClient from 'apollo-boost';
-import { gql } from "apollo-boost";
+import { gql } from 'apollo-boost';
+import SwipeCard from '../components/SwipeCard';
 
-export default function SwipeScreen() {
-    const client = new ApolloClient({
-      uri: 'https://www.graphqlhub.com/graphql',
-    });
+class SwipeScreen extends React.Component {
+    static navigationOptions = {
+        header: 'pupper',
+    };
 
-    client.query({
-        query: gql `{
-            reddit {
-                subreddit(name: "darkmeme"){
-                    hotListings(limit: 19) {
-                            fullnameId
-                            title
-                            score
-                            url
+    constructor(props) {
+        super(props);
+        this.state = {
+            cardValues: []
+        };
+
+    }
+
+    componentWillMount() {
+        this.graphqlRequest();
+    }
+
+    graphqlRequest() {
+        console.log(this.state.cardValues)
+
+        let client = new ApolloClient({
+            uri: 'https://www.graphqlhub.com/graphql',
+        });
+
+        client.query({
+            query: gql `{
+                reddit {
+                    subreddit(name: "darkmeme"){
+                        hotListings(limit: 19) {
+                                fullnameId
+                                title
+                                score
+                                url
+                            }
                         }
-                    }
-            }
-        }`
-    }).then(result => console.log(result))
+                }
+            }`
+        }).then((result) => {
+            this.setState({
+                cardValues: result.data.reddit.subreddit.hotListings || {}
+            })
+        })
+    }
 
+    render() {
+        return (
+            <View>
+                {(this.state.cardValues).map((prop) => {
+                    return (
+                        <SwipeCard />
+                    );
+                })}
 
-    return (
-        <View>
-            <Text> Like me! </Text>
-        </View>
-    );
+                <Text> Like me! </Text>
+            </View>
+        );
+    }
 }
 
-SwipeScreen.navigationOptions = {
-    header: 'pupper',
-};
+export default SwipeScreen;
