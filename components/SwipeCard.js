3531cebf76d713f287c0ab984ec5d2ff80ca18fb
Toprak <hello@toprak.dev>
2020-03-20 04:00:10 +0300

    created basic card structure

--- components/SwipeCard.js
+++ components/SwipeCard.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Dimensions, Animated, PanResponder } from 'react-native';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class SwipeCard extends Component {
+    constructor(props) {
+        super(props);
+        this.cardDetails = JSON.parse(this.props.cardDetails);
+        this.position = new Animated.ValueXY();
+    }
+
+    componentWillMount() {
+        this.PanResponder = PanResponder.create({
+            onStartShouldSetPanResponder: (event, gestureState) => true,
+            onPanResponderMove: (event, gestureState) => {
+                this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
+            },
+            onPanResponderRelease: (event, gestureState) => {
+
+            }
+        });
+    }
 
     render() {
         return (
-            <View>
-                <Text>Hey, there !</Text>
-            </View>
+            <Animated.View {...this.PanResponder.panHandlers} style={[{transform: this.position.getTranslateTransform() },{ height: SCREEN_HEIGHT-120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' }]} >
+                <Text>{this.cardDetails.title}</Text>
+                <Image style={{ flex: 1, width: null, height: null, resizeMode: 'cover', borderRadius: 20 }} source={{uri: this.cardDetails.url}}/>
+            </Animated.View>
         );
     }
 }
